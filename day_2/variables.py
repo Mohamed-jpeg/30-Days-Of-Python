ea8f72e732deb93e48e1f224e1013798f4f82682
Mohamed-jpeg <Mouhamed.Cherif@esprit.tn>
2026-07-07 00:34:29 +0100

    day 2 finished

---
+++ day_2/variables.py
@@ -0,0 +1,59 @@
+#Day 2: 30 Days of python programming
+first_name = 'Med'
+last_name = "cherif"
+full_name = "med cherif"
+country ="tunisia"
+city ="menzah 5"
+age =20
+year =2026
+is_married= False #inchalah true
+is_true=True
+is_light=True
+med = 12
+cherif = 23
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+print(type(med))
+
+print(len(first_name))
+
+if (len(first_name)>len(last_name)):
+    print(first_name)
+else:
+    print(last_name)
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+r=30
+area_of_circle = 3.14 * r * r
+circum_of_circle = 2 * 3.14 * r
+
+radius = float(input("Enter radius: "))
+area_of_circle_from_input = 3.14 * radius * radius
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = input("Enter your age: ")
+
+help('keywords')
+
+
